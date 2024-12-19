f770456344dab0529bfd3a959e3808245975087e
Anushka-Jayasooriya <jayasooriya.adv@gmail.com>
2024-12-20 01:27:25 +0530

    added ips

--- services/appointment_service/app/config.py
+++ services/appointment_service/app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    PATIENT_SERVICE_URL = os.getenv("PATIENT_SERVICE_URL", "http://127.0.0.1:8001")
-    DOCTOR_SERVICE_URL = os.getenv("DOCTOR_SERVICE_URL", "http://127.0.0.1:8002")
-    NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://127.0.0.1:8005")
+    PATIENT_SERVICE_URL = os.getenv("PATIENT_SERVICE_URL", "http://34.71.102.253:8001")
+    DOCTOR_SERVICE_URL = os.getenv("DOCTOR_SERVICE_URL", "http://34.44.196.226:8002")
+    NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://34.134.196.5:8005")
 
 settings = Settings()
