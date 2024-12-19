be7902ab8b3dd472c98abcc8b4d30ae9fe2faf80
Anushka-Jayasooriya <jayasooriya.adv@gmail.com>
2024-12-20 01:04:34 +0530

    fixes

--- services/aggregator_service/config.py
+++ services/aggregator_service/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     # MongoDB Settings
     MONGO_URL = os.getenv('MONGO_URL', 'mongodb://localhost:27017')
-    MONGO_DB = os.getenv('MONGO_DB', 'health')
+    MONGO_DB = os.getenv('MONGO_DB_NAME', 'healthsync')
 
     # Redshift Settings
     REDSHIFT_DB = os.getenv('REDSHIFT_DB', 'healthsync')
